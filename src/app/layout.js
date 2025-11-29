9cb080d88452b7e4fddb1f7211a332eb987fdf8e
Adonis Davis <ace6k@icloud.com>
2025-11-29 12:24:04 -0500

    Updated app icons

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,11 @@ import './globals.css';
 
 export const metadata = {
   title: "Punch Card",
-  description: "Boxing Scorecard App"
+  description: "Boxing Scorecard App",
+  icons: {
+    icon: "/icon.png",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
